c698e54e5a1578f321d1ee757cb881c37858d889
Shookas <piotr_szukala@o2.pl>
2018-10-30 21:49:41 +0100

    adjust top menu

--- src/app/router.module.ts
+++ src/app/router.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MapContainerComponent } from './map-viewer/map-container/map-container.component';
 import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
 import { AuthGuard } from './auth/auth.guard';
 
 const appRoutes: Routes = [
